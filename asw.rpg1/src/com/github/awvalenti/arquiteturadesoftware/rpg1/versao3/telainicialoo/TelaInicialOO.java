7ff74556fb3ff4efd4c6f33179b374db60c4d859
Fábio Santos Valonga <fabiosv.etep@gmail.com>
2015-03-10 21:57:06 -0300

    criado o metodo alterarElemento

--- asw.rpg1/src/com/github/awvalenti/arquiteturadesoftware/rpg1/versao3/telainicialoo/TelaInicialOO.java
+++ asw.rpg1/src/com/github/awvalenti/arquiteturadesoftware/rpg1/versao3/telainicialoo/TelaInicialOO.java
@@ -8,15 +8,19 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import com.github.awvalenti.arquiteturadesoftware.rpg1.versao1.procedimental.Main;
+
 public class TelaInicialOO {
+	final JFrame frame = new JFrame();
+	private int numeroLinhas;
+	private int numeroColunas;
 
 	public TelaInicialOO(Elemento[][] disposicaoInicial) throws IOException {
 
-		final JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		int numeroLinhas = disposicaoInicial.length;
-		int numeroColunas = disposicaoInicial[0].length;
+		this.numeroLinhas = disposicaoInicial.length;
+		this.numeroColunas = disposicaoInicial[0].length;
 
 		frame.setLayout(new GridLayout(numeroLinhas, numeroColunas));
 
@@ -33,7 +37,12 @@ public class TelaInicialOO {
 	}
 
 	public void alterarElemento(int linha, int coluna, Elemento novoElemento) {
-		// ??
+		try {
+			((JLabel) frame.getContentPane().getComponent(linha * numeroColunas + coluna)).setIcon(new ImageIcon(ImageIO.read(Main.class.getResourceAsStream(novoElemento.getCaminhoImagem()))));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
