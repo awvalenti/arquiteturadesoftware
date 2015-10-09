8df7e44b16986a8cbe84554bb814af9864bc3b7a
Andre Willik Valenti <awvalenti@gmail.com>
2015-10-08 22:42:07 -0300

    Apos dojo 2

--- asw.mensageiro/src/asw/mensageiro/versao2/emcamadas/CamadaNegocio.java
+++ asw.mensageiro/src/asw/mensageiro/versao2/emcamadas/CamadaNegocio.java
@@ -1,5 +1,8 @@
 package asw.mensageiro.versao2.emcamadas;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CamadaNegocio {
 
 	private CamadaDados dados;
@@ -7,5 +10,17 @@ public class CamadaNegocio {
 	public CamadaNegocio(CamadaDados dados) {
 		this.dados = dados;
 	}
-
+	public List<String> listarMensagens(){
+		return Arrays.asList(dados.getDiretorio().list());		
+	}
+	
+	public String lerMensagem(String nomeMsg) throws MensagemNaoEncontradaException {
+		return dados.lerMensagem(nomeMsg);		
+	}
+	public boolean criarMensagem(String nomeMsg, String conteudo) {
+		return dados.criarMensagem(nomeMsg, conteudo);
+	}
+	public void excluir (String nomeMsg) {
+		dados.excluir(nomeMsg);
+	}
 }
